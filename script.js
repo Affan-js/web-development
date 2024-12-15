99be2a33093ea17167ba11eb03e8c83fe704d40d
Affan <111012151+Affan-js@users.noreply.github.com>
2024-12-15 16:36:23 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculateVolume() {
+    const height = parseFloat(document.getElementById("height").value);
+    const width = parseFloat(document.getElementById("width").value);
+    const depth = parseFloat(document.getElementById("depth").value);
+
+    const volume = height * width * depth;
+
+    document.getElementById("result").textContent = `Volume: ${volume}`;
+}
